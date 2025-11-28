fbb5e9c7375c3ffce27fe2748065dac47e7966fa
P0tatOo <128663126+P0tatOo@users.noreply.github.com>
2025-11-29 05:36:29 +0900

    added creating account page

--- app/src/main/java/com/example/myapplication/ui/check/CheckNameActivity.kt
+++ app/src/main/java/com/example/myapplication/ui/check/CheckNameActivity.kt
@@ -3,8 +3,10 @@ package com.example.myapplication.ui.check
 import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
--- app/src/main/java/com/example/myapplication/ui/main/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/ui/main/MainActivity.kt
@@ -1,17 +1,23 @@
-package com.example.myapplication
+package com.example.myapplication.ui.main
 
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
-import com.example.myapplication.ui.idcard.IdCardRecognitionActivity
+import androidx.cardview.widget.CardView
+import com.example.myapplication.R
+import com.example.myapplication.ui.idcard.CreateAccountActivity
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var cameraButton: Button
+    private lateinit var createAccount: CardView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        createAccount = findViewById(R.id.create_account)
+        createAccount.setOnClickListener {
+            startActivity(Intent(this, CreateAccountActivity::class.java))
+        }
 
 //        cameraButton = findViewById(R.id.camera_button)
 //        cameraButton.setOnClickListener {
