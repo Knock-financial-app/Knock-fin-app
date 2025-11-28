8932f3d30365c9be5ce23bb53ace7b17e66a7d78
Widadada <jeong7571@knu.ac.kr>
2025-11-29 04:24:18 +0900

    feat: 발급일자 확인 페이지 구현

--- app/src/main/java/com/example/myapplication/ui/check/CheckNameActivity.kt
+++ app/src/main/java/com/example/myapplication/ui/check/CheckNameActivity.kt
@@ -6,6 +6,7 @@ import android.os.Bundle
 import android.util.Log
 import android.view.View
 import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
